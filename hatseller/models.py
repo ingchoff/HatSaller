1a852d747375600c63c66ebd8c5c3c5bebc8c2d7
Phuree Kanusont <60070075@kmitl.ac.th>
2019-05-22 23:23:19 +0700

    add hat model

--- hatseller/models.py
+++ hatseller/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class hat(models.Model):
+    title = models.CharField(max_length=30)
+    stock = models.IntegerField(default=1)
+    description = models.TextField(null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField()
+
+    def __str__(self):
+        return "%s by %s" % (self.title, self.created_by)
