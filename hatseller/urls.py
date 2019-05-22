2953dd2ce34bb18b1658c69d9b2123376d702da7
ingchoff <ingkaratt@gmail.com>
2019-05-23 00:09:40 +0700

    Fixed login logout

--- hatseller/urls.py
+++ hatseller/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('login/', views.my_login, name='login')
+    path('login/', views.my_login, name='login'),
+    path('logout/', views.my_logout, name='logout')
 ]
