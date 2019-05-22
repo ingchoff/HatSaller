683ff247a01f25870ba9dc698f5ac1e8879cdd96
Phuree Kanusont <60070075@kmitl.ac.th>
2019-05-23 00:16:22 +0700

    add register function

--- hatseller/forms.py
+++ hatseller/forms.py
@@ -1,7 +1,15 @@
 from django import forms
+from django.core import validators
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import authenticate
 from django.http import request
 
+def validate_password(value):
+    if len(value) < 8:
+        raise ValidationError(
+            _('password must have more than 8 characters'),
+        )
 
 class MyLoginForm(forms.Form):
     user = forms.CharField(max_length=100, label='Username:')
@@ -23,3 +31,76 @@ class MyLoginForm(forms.Form):
         user = authenticate(request, username=username, password=password)
         if user is None:
             raise forms.ValidationError('Wrong username or password!')
+
+class RegisterForm(forms.Form):
+    first_name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'First Name'
+            }
+        ),
+        required=False
+    )
+
+    last_name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Last Name'
+            }
+        ),
+        required=False
+    )
+
+    email = forms.EmailField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'E-mail'
+            }
+        ),
+        required=False,
+        validators=[validators.EmailValidator]
+    )
+    
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Password'
+            }
+        ),
+        validators=[validate_password]
+    )
+
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Password'
+            }
+        ),
+        validators=[validate_password]
+    )
+
+    def clean_first_name(self):
+        data = self.cleaned_data['first_name']
+        if len(data) == 0:
+            raise forms.ValidationError("โปรดกรอกชื่อจริง")
+
+    def clean_last_name(self):
+        data = self.cleaned_data['last_name']
+        if len(data) == 0:
+            raise forms.ValidationError("โปรดกรอกชื่อจริง")
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        con_password = cleaned_data.get('confirm_password')
+
+        if password != con_password:
+            raise forms.ValidationError('password ไม่เหมือนกัน')
--- hatseller/views.py
+++ hatseller/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from hatseller.forms import MyLoginForm
+from hatseller.forms import MyLoginForm, RegisterForm
 
+from django.contrib.auth.models import User
 
 def index(request):
     context = {
@@ -24,3 +25,23 @@ def my_login(request):
 
     context['form'] = form
     return render(request, 'hatseller/login.html', context=context)
+
+def register(request):
+    context = {}
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data.pop('password')
+            user = User(**form.cleaned_data)
+            user.set_password(password)
+            user.save()
+            return redirect('views.index', context=context)
+    else:
+        form = RegisterForm()
+    #     next_url = request.GET.get('next')
+    #     context['form'] = form
+    #     if next_url:
+    #         context['next_url'] = next_url
+
+    context['form'] = form
+    return render(request, 'hatseller/register.html', context=context)
